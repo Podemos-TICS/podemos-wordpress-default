fc50093c6af83961e9c0381f1d9ff3b043d81fc6
Jorge Valencia <jorgev@jorgev.es>
2014-08-11 18:29:54 +0200

    Cambiada segunda referencia al json
    Sigo sin haber podido probar si funciona con https

--- functions.php
+++ functions.php
@@ -1613,5 +1613,5 @@ function podemos_register_required_plugins() {
 require 'theme-updates/theme-update-checker.php';
 $example_update_checker = new ThemeUpdateChecker(
     'podemos-wordpress-default-master',
-    'http://info.pinternet.es/actualizacionespodemos/wp-podemos.json'
+    'https://github.com/Podemos-TICS/podemos-wordpress-default/blob/master/wp-podemos.json'
 );
